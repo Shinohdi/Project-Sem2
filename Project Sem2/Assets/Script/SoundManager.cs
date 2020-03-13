ebc53a629b09fe60de0bcb850371691c395ab78a
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2020-03-13 23:14:55 +0100

    IntégrationSonore

--- Project Sem2/Assets/Script/SoundManager.cs
+++ Project Sem2/Assets/Script/SoundManager.cs
@@ -5,6 +5,34 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
 
+    [FMODUnity.EventRef]
+    public string Event;
+    FMOD.Studio.EventInstance Audio;
+
+    void Awake()
+    {
+        Audio = FMODUnity.RuntimeManager.CreateInstance(Event);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "TexturePeinture")
+        {
+            Debug.Log("Paint");
+            Audio.setParameterByName("PasAvecPeinture", 1);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "TexturePeinture")
+        {
+            Debug.Log("No Paint");
+            Audio.setParameterByName("PasAvecPeinture", 0);
+        }
+    }
+
     public void PasSurSurfaceSimple(string path)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
