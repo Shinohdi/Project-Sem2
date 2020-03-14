f4c66bb8ab8a411073bdf948db52d57e195a5b7b
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2020-03-14 09:58:11 +0100

    Update SoundManager.cs

--- Project Sem2/Assets/Script/SoundManager.cs
+++ Project Sem2/Assets/Script/SoundManager.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
 
-    [FMODUnity.EventRef]
+    /*[FMODUnity.EventRef]
     public string Event;
-    FMOD.Studio.EventInstance Audio;
+    FMOD.Studio.EventInstance Audio;*/
 
-    void Awake()
+    public bool surPeinture;
+
+    /*void Awake()
     {
         Audio = FMODUnity.RuntimeManager.CreateInstance(Event);
-    }
+    }*/
 
     void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "TexturePeinture")
         {
             Debug.Log("Paint");
-            Audio.setParameterByName("PasAvecPeinture", 1);
+            /*Audio.setParameterByName("PasAvecPeinture", 1);*/
 
+            surPeinture = true;
         }
     }
 
@@ -29,12 +32,22 @@ public class SoundManager : MonoBehaviour
         if (other.gameObject.tag == "TexturePeinture")
         {
             Debug.Log("No Paint");
-            Audio.setParameterByName("PasAvecPeinture", 0);
+            /*Audio.setParameterByName("PasAvecPeinture", 0);*/
+
+            surPeinture = false;
         }
     }
 
-    public void PasSurSurfaceSimple(string path)
+    public void PasSurSurfaceSimple()
     {
-        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+        if (surPeinture == false)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Avatar/Deplacements/SurfaceSimple/Pas", transform.position);
+        }
+        if(surPeinture == true)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Avatar/Deplacements/SurfaceAvecPeinture/PasSurPeinture", transform.position);
+        }
+        
     }
 }
