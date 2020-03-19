b732a34ddb64818947eebd62c8ca2859aefe1331
Shinohdi <62138018+Shinohdi@users.noreply.github.com>
2020-03-19 15:48:36 +0100

    Fonction de peinture

---
+++ Project Sem2/Assets/TexturePaint/ClickScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour {
+    public Camera vision;
+    public Transform player;
+    public Texture2D splashTexture;
+	
+	void Update ()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = vision.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(player.position, vision.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name);
+                MyShaderBehavior script = hit.collider.gameObject.GetComponent<MyShaderBehavior>();
+                if (null != script)
+                    script.PaintOn(hit.textureCoord, splashTexture);
+            }
+        }
+	}
+}
