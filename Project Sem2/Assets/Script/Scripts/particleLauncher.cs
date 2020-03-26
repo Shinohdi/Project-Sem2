5cba7fc711e3601360aaf2b2fc614c475a29cf9d
Ralphh23 <61753268+Ralphh23@users.noreply.github.com>
2020-03-26 15:14:27 +0100

    Graph_boule
    Udpate
    nouvelle scène

---
+++ Project Sem2/Assets/Script/Scripts/particleLauncher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleLauncher : MonoBehaviour
+{
+    public ParticleSystem ParticleLauncher;
+    public ParticleSystem splatterParticles;
+    public Gradient particleColorGradient;
+    public ParticleDecalPool splatDecalPool;
+
+    List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ParticlePhysicsExtensions.GetCollisionEvents(ParticleLauncher, other, collisionEvents);
+
+        for (int i = 0; i < collisionEvents.Count; i++)
+        {
+            splatDecalPool.ParticleHit(collisionEvents[i], particleColorGradient);
+            EmitAtLocation(collisionEvents[i]);
+        }
+        
+    }
+
+    void EmitAtLocation(ParticleCollisionEvent particleCollisionEvent)
+    {
+        splatterParticles.transform.position = particleCollisionEvent.intersection; // worldspace
+        splatterParticles.transform.rotation = Quaternion.LookRotation(particleCollisionEvent.normal);
+        ParticleSystem.MainModule psMain = splatterParticles.main;
+        psMain.startColor = particleColorGradient.Evaluate(Random.Range(0f, 1f));
+
+        splatterParticles.Emit(1);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            ParticleSystem.MainModule psMain = ParticleLauncher.main;
+            psMain.startColor = particleColorGradient.Evaluate(Random.Range(0f,1f));
+            ParticleLauncher.Emit(1);
+        }
+
+    }
+}
