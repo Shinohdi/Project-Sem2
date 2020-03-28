f092f832cd0127430d7ffafad7888c8176a6dbdc
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2020-03-28 23:23:38 +0100

    Integration son spray

---
+++ Project Sem2/Assets/Script/ScriptsPourSon/SoundOnPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FMODUnity
+{
+    public class SoundOnPlayer : MonoBehaviour
+    {
+        [EventRef]
+        public string EventTir = "";
+        [EventRef]
+        public string EventRelacher = "";
+
+
+        FMOD.Studio.EventInstance tir;
+
+        public bool AllowFadeout = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            tir = FMODUnity.RuntimeManager.CreateInstance(EventTir);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                tir.start();
+            }
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                FMODUnity.RuntimeManager.PlayOneShot(EventRelacher, transform.position);
+                tir.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            }
+        }
+    }
+}
+
+
