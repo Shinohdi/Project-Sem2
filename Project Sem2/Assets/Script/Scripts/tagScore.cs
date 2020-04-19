f34bd07e1b23341ce53d38545f489fd9e258111c
Shinohdi <62138018+Shinohdi@users.noreply.github.com>
2020-04-19 16:01:28 +0200

    Scoring update

---
+++ Project Sem2/Assets/Script/Scripts/tagScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tagScore : MonoBehaviour
+{
+    public bool isScoring;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("score"))
+        {
+            isScoring = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("score"))
+        {
+            isScoring = false;
+        }
+    }
+}
