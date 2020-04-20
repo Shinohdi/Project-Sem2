b18b6f6fe32d8c94f2061556746a54c9baa75eea
Xenoyorshka <62139908+Xenoyorshka@users.noreply.github.com>
2020-04-21 00:25:40 +0200

    le spray

---
+++ Project Sem2/Assets/Prefabs/SprayMat/push.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    public Animator Animator;
+
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Animator.SetBool("bool", true);
+        }
+        else
+        {
+            Animator.SetBool("bool", false);
+        }
+
+    }
+}
