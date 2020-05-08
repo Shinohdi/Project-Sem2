2edf1fb2cf7ff3b67a2879420ccb00650862b6ca
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2020-05-08 12:06:07 +0200

    TestsSons

---
+++ Project Sem2/Assets/Script/ScriptsPourSon/TestSonIsTriggered.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSonIsTriggered : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Avatar"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 20, gameObject.transform.position.z);
+        }
+    }
+}
