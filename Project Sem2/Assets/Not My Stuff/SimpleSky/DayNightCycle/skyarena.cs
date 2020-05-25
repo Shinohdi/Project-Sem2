ddca8b0c99cb744b403c246bbf01279e6e96f45c
Ralphh23 <61753268+Ralphh23@users.noreply.github.com>
2020-05-25 23:29:10 +0200

    skybox nuit/jour

---
+++ Project Sem2/Assets/Not My Stuff/SimpleSky/DayNightCycle/skyarena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyarena : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 0.1f;
+    private float offset = 0.5f;
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += Time.deltaTime * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
