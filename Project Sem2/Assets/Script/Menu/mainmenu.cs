55afef31e42143a9d7520287e33e7767dd80df4e
Ralphh23 <61753268+Ralphh23@users.noreply.github.com>
2020-05-27 23:55:30 +0200

    Menu

---
+++ Project Sem2/Assets/Script/Menu/mainmenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainmenu : MonoBehaviour
+{
+    public void Playgame ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quitgame ()
+    {
+        Debug.Log("Quitter;");
+        Application.Quit();
+
+    }
+
+
+
+
+}
