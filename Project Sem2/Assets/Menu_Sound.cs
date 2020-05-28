a0c87986734e5450bb31b5d996da51ca31705770
ArcaTsu <57213316+ArcaTsu@users.noreply.github.com>
2020-05-28 10:54:21 +0200

    Sons sur la scene Menu

---
+++ Project Sem2/Assets/Menu_Sound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class Menu_Sound : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string EventAmbiance;
+    FMOD.Studio.EventInstance Ambiance;
+
+    [FMODUnity.EventRef]
+    public string EventOnClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ambiance = FMODUnity.RuntimeManager.CreateInstance(EventAmbiance);
+        Ambiance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlay()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(EventOnClick, transform.position);
+        Ambiance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
